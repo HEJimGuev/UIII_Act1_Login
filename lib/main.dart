fa52ce900e76b5d65c15336788e38a89340ed994
Haydee <a.21308051280493@cbtis128.edu.mx>
2024-06-03 16:45:18 +0000

    Actualizando

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,154 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(Login());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class Login extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       // Application name
-      title: 'Flutter Hello World',
+      title: 'Login',
       // Application theme data, you can set the colors for the application as
       // you want
       theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
+          // useMaterial3: false,
+
+          visualDensity: VisualDensity.adaptivePlatformDensity),
       // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Container(
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage("assets/background.jpg"), fit: BoxFit.cover)),
+        child: Scaffold(
+          backgroundColor: Colors.transparent,
+          body: ListView(
+            children: [
+              SizedBox(height: 50),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    "assets/Logo_coleccionistaS.png",
+                    width: 150,
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding: EdgeInsets.all(25),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20),
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.grey,
+                          blurRadius: 10,
+                          offset: Offset(0, 5))
+                    ],
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        padding: EdgeInsets.all(10),
+                        child: Text("OHH",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 25,
+                              color: Color(0x515e40ca),
+                            )),
+                      ),
+                      Container(
+                        padding: EdgeInsets.all(5),
+                        child: Text(
+                          "Es bueno verte de nuevo.",
+                          style: TextStyle(fontSize: 20),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 30,
+                      ),
+                      Padding(
+                        padding:
+                            EdgeInsets.symmetric(vertical: 0, horizontal: 30),
+                        child: TextField(
+                          autofocus: false,
+                          decoration: InputDecoration(
+                              hintText: "Usuario",
+                              prefixIcon: Icon(Icons.person),
+                              contentPadding: EdgeInsets.all(10),
+                              hintStyle:
+                                  TextStyle(color: Colors.grey, fontSize: 18),
+                              enabledBorder: UnderlineInputBorder(
+                                  borderSide: BorderSide(
+                                      color: Colors.grey, width: 2))),
+                        ),
+                      ),
+                      SizedBox(height: 40),
+                      Padding(
+                        padding:
+                            EdgeInsets.symmetric(vertical: 0, horizontal: 30),
+                        child: TextField(
+                          autofocus: false,
+                          decoration: InputDecoration(
+                              hintText: "Contraseña",
+                              prefixIcon: Icon(Icons.security),
+                              contentPadding: EdgeInsets.all(10),
+                              hintStyle:
+                                  TextStyle(color: Colors.grey, fontSize: 18),
+                              enabledBorder: UnderlineInputBorder(
+                                  borderSide: BorderSide(
+                                      color: Colors.grey, width: 2))),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 60,
+                      ),
+                      InkWell(
+                        onTap: () {},
+                        child: Container(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Padding(
+                                padding: EdgeInsets.all(20),
+                                child: Text(
+                                  "INICIA SESION",
+                                  style: TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              )
+                            ],
+                          ),
+                          decoration: BoxDecoration(
+                              color: Color(0x510194d8),
+                              borderRadius: BorderRadius.only(
+                                  bottomLeft: Radius.circular(20),
+                                  bottomRight: Radius.circular(20))),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
   final String title;
-  const MyHomePage({super.key, required this.title});  
+  const MyHomePage({super.key, required this.title});
 
   @override
   Widget build(BuildContext context) {
